bf627ab7fc5cd700bd51c90ccb47511b60ee64c8
zouxiang <zouxiang@beidd.com>
2019-03-15 10:59:44 +0800

    修改静态文件

---
+++ static/shareClassCode/static/config.js
@@ -0,0 +1,43 @@
+/**
+ * Author zouxiang
+ * Create Time: 2019-03-15
+ * Description:
+ */
+window.class_code_config = [
+  {
+    name: '一年级',
+    code: './static/class1.png'
+  },
+  {
+    name: '二年级',
+    code: './static/class2.png'
+  },
+  {
+    name: '三年级',
+    code: './static/class3.png'
+  },
+  {
+    name: '四年级',
+    code: './static/class4.png'
+  },
+  {
+    name: '五年级',
+    code: './static/class5.png'
+  },
+  {
+    name: '六年级',
+    code: './static/class6.png'
+  },
+  {
+    name: '七年级',
+    code: './static/class7.png'
+  },
+  {
+    name: '八年级',
+    code: './static/class8.png'
+  },
+  {
+    name: '九年级',
+    code: './static/class9.png'
+  }
+]
