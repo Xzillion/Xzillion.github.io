305c4bef41cdb63de0839447220c9ba7db1ab198
xzillion <zouxiang13148@outlook.com>
2019-03-20 23:51:34 +0800

    更新静态文件

--- static/shareClassCode/static/config.js
+++ static/shareClassCode/static/config.js
@@ -6,29 +6,26 @@
 window.class_code_config = [
   {
     name: '1~4年级',
-    code: './static/class1_4.jpg'
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c92520caf601f06bfb247c4'
   },
   {
     name: '五年级',
-    code: './static/class5.jpg'
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c925446af601f06c05b832f'
   },
   {
     name: '六年级',
-    code: './static/class6.jpg'
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c925489af601f06c2173d59'
   },
   {
     name: '七年级',
-    code: './static/class7.jpg'
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c9254adaf601f06c174713d'
   },
   {
     name: '八年级',
-    code: './static/class8.jpg'
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c9254c3af601f06c2173d5c'
   },
   {
     name: '九年级及以上',
-    code: './static/class9+.jpg'
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c9254deaf601f06bfb247ca'
   }
 ]
-window.class_code_config.forEach(function (item) {
-  item.code = item.code + '?version=' + Date.now()
-})
