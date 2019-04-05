d3a04b1a4d4c3b9a228576620b7f082d1bfcaa45
xzillion <zouxiang13148@outlook.com>
2019-04-05 12:28:34 +0800

    更新静态文件

---
+++ static/shareClassCode2/static/config.js
@@ -0,0 +1,31 @@
+/**
+ * Author zouxiang
+ * Create Time: 2019-03-15
+ * Description:
+ */
+window.class_code_config = [
+  {
+    name: '1~4年级',
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c92520caf601f06bfb247c4'
+  },
+  {
+    name: '五年级',
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c925446af601f06c05b832f'
+  },
+  {
+    name: '六年级',
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c925489af601f06c2173d59'
+  },
+  {
+    name: '七年级',
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c9254adaf601f06c174713d'
+  },
+  {
+    name: '八年级',
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c9254c3af601f06c2173d5c'
+  },
+  {
+    name: '九年级及以上',
+    code: 'http://promotion.hmq9.cn/index.html?lid=5c9254deaf601f06bfb247ca'
+  }
+]
